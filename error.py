8d8e3680ed67d548b89a917755c4aeefcad64587
Scholar <scholar@paradigm.local>
2023-08-29 13:32:49 -0600

    created a charecter

--- error.py
+++ error.py
@@ -1,2 +1,10 @@
 # here is my first file
-print('hello')
+#print('hello')
+user_name = input ('What is your name?')
+
+character = {
+    "name": "Walt Disney",
+    "age": 63,
+    "isAlive": False,
+    "fave_food_list":['cookies', 'hot dogs', 'pretzels', 'ice cream', 'hamburgers']
+}
