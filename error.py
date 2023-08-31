c1015ce038a36ebcbdcffaf8e433e62e48c3fa43
Scholar <scholar@paradigm.local>
2023-08-31 12:26:38 -0600

    created character

--- error.py
+++ error.py
@@ -1,10 +1,8 @@
 # here is my first file
-#print('hello')
-user_name = input ('What is your name?')
 
 character = {
     "name": "Walt Disney",
     "age": 63,
-    "isAlive": False,
-    "fave_food_list":['cookies', 'hot dogs', 'pretzels', 'ice cream', 'hamburgers']
+    "isMale": True,
+    "fav_food_list": ['cookies', 'hot dogs', 'pretzels', 'ice cream', 'hamburgers']
 }
