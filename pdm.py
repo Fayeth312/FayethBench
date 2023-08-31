e2cb54197398e8e04103755a910a78f99e86a74a
Scholar <scholar@paradigm.local>
2023-08-31 13:23:29 -0600

    character created

---
+++ pdm.py
@@ -0,0 +1,29 @@
+person = "Barret"
+
+for i in range(10):
+    print(i)
+
+    list = ['one', 'two', 'three']
+    dictionary = {
+        'name': 'Barret',
+        'age': 16
+    }
+
+
+    def is_even_number(number):
+        if number % 2 ==0:
+            print('This is an even number!')
+        else: 
+            print ('This is an odd number')
+
+is_even_number(42)
+
+MAIN_CHARECTER = "Mario"
+
+main_character = "Luigi"
+
+myName = "Fayeth" #camel case
+
+my_name = "Name" #snake case
+
+MyName = "YN"  #pascal case
