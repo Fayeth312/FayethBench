cf09b3be4ce0d7307a755811e443c9c3f91586ac
Scholar <scholar@paradigm.local>
2023-09-12 13:29:59 -0600

    simple calculator

---
+++ simple calculator.py
@@ -0,0 +1,7 @@
+user_input = input("Insert first number: ")
+user_input2 = input("Insert second number: ")
+user_input3 = input("What operation are you using?: +, -, *, /: ")
+
+def input(user_input3):
+    if user_input3 == "+":
+        print(user_input + user_input2)
