42e6209503ea3a9d3f977b92ed632528c8bbf9aa
Scholar <scholar@paradigm.local>
2023-09-14 13:27:36 -0600

    functions

---
+++ functions.py
@@ -0,0 +1,27 @@
+#built in function
+#function
+#def print_my_name(name):
+   # print(name)
+#function call
+#print_my_name('Bobtacular')
+
+#def trip_planner(start, end, duration, mode = 'car'):
+    #print(f'It looks like your starting a trip from {start}')
+    #print(f'you are planning to get to {end}')
+    #print(f'It should take you about {duration} hours')
+    #print(f'I see you are taking a {mode}')
+
+#trip_planner('Paradigm', 'Disneyland', 2, 'plane')
+
+def print_my_name(name):
+    print(name)
+
+print_my_name('Fayeth')
+
+x = "fun"
+
+def print_global_variable():
+  print("Coding is " + x)
+
+print_global_variable()
+
--- simple calculator.py
+++ simple calculator.py
@@ -1,7 +1,27 @@
-user_input = input("Insert first number: ")
-user_input2 = input("Insert second number: ")
+user_input = int(input("Insert first number: "))
+user_input2 = int(input("Insert second number: "))
 user_input3 = input("What operation are you using?: +, -, *, /: ")
 
-def input(user_input3):
-    if user_input3 == "+":
-        print(user_input + user_input2)
+def add():
+    print(f'{user_input} + {user_input2} equals {user_input + user_input2}')
+
+def subtract():
+    print(f'{user_input} - {user_input2} equals {user_input - user_input2}')
+
+def multiply():
+    print(f'{user_input} * {user_input2} equals {user_input * user_input2}')
+
+def divide():
+    print(f'{user_input} / {user_input2} equals {user_input / user_input2}')
+
+if user_input3 == "+":
+    add()
+     
+if user_input3 == "-":
+    subtract()
+      
+if user_input3 == "*":
+    multiply()
+        
+if user_input3 == "/":
+    divide()
