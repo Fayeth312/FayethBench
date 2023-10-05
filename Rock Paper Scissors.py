28dff146381072216cbbedc393a89d4d0f3fdee0
Scholar <scholar@paradigm.local>
2023-10-05 13:25:22 -0600

    rock paper scissors

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,20 @@
+user_input1 = input("Player 1; rock, paper or scissors?: ")
+user_input2 = input("Player 2; rock, paper or scissors?: ")
+
+if  user_input1 > user_input2:
+    print(f"")
+elif  user_input1 > user_input2:
+    print(f"")
+elif  user_input1 > user_input2:
+    print(f"")
+
+
+if  user_input1 > user_input2:
+    print(f"")
+elif  user_input1 > user_input2:
+    print(f"")
+elif  user_input1 > user_input2:
+    print(f"")
+
+if user_input1 == user_input2:
+    print(f"Error, try again")
