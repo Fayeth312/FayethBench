02e6bb37070a2679f28ed2c11c68bcda4f7d8bcb
Scholar <scholar@paradigm.local>
2023-10-31 13:32:19 -0600

    interveiw

---
+++ job interveiw.py
@@ -0,0 +1,60 @@
+name = input("Welcome. Before we we start this interveiw, what is your name? ")
+start_interview = print (f"Alright {name}! Let's start.")
+first = input ("What type of job would you like? ")
+second = input ("How many years of experience do you have with this job? ")
+third = input ("What is your salary you would like you make? ")
+fourth = print (f"You said you would like to be a {first}. Why?")
+user = input("" )
+small_talk = print ("Good to know.")
+fith = input ("Tell me some of your greatest strengths. What are they? ")
+sixth = input ("How do you normally treat others? ")
+seventh = print(f"What are your goals by being a {first}? ")
+user2 = input ("" )
+eighth = input ("Is this your first job? ")
+ninth = input ("How quick do you pick up new skills? ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print{f"thank you {name} for attending this interciew. I want to recap what we went over. Does the sound correct?"}
