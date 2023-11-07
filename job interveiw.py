d5db953a595e0ca8b537e9a147d28792dd37d610
Scholar <scholar@paradigm.local>
2023-11-07 13:29:21 -0700

    job interveiw

--- job interveiw.py
+++ job interveiw.py
@@ -11,11 +11,10 @@ sentance1 = print("Good to know. ")
 third = int(input("What is your salary you would like you make? "))
 
 if third > 50000:
-    print("I'm sorry. We don't have that kind of money.")
+    print("I'm sorry. We don't have that amout of money to give.")
     exit()
 
-fourth = print (f"You said you would like to be a {first}. Why?")
-user = input("" )
+fourth = input(f"You said you would like to be {first}. Why?")
 small_talk = print ("Alright, good to know.")
 fifth = input("What are three of your greatest strengths? ")
 sixth = int(input("How old are you? "))
@@ -24,8 +23,13 @@ if third < 14:
     print("I'm sorry, you need to be 14 or older. Try again when your old enough.")
     exit()
 
-seventh = print(f"What are your goals by being a {first}? ")
-user2 = input("" )
+seventh = input(f"What are your goals by being {first}? ")
 eighth = input("Is this your first job? ")
 ninth = input("What motivates you? ")
 tenth = input("Why should I hire you? " )
+recap = input(f"Alright {name}, congratualations on making it through the interveiw. I would like to reacap what we have gone over. You would like to be a {first}, you have {second} years of experience, you want your salary to be {third}, you want this job because {fourth}, your greatest strengths are {fifth}, you are {sixth} years old, your goals with being a {first} are {seventh}, was this your first job? {eighth}, {ninth} motivates you, and I should hire you because {tenth}. Is this right? Yes or No: ")
+
+if recap == "yes" or "Yes": 
+    print ("Alright. You are hired.")
+else:
+    print ("Alright. I'll look through my notes. I will contact you later.")
