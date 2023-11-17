7a3c4fc58c20aef849664460d6a20d5fc190b97a
Scholar <scholar@paradigm.local>
2023-11-17 11:23:34 -0700

    guess a word

--- guess a word.py
+++ guess a word.py
@@ -1,4 +1,4 @@
-rules = print ("Welcome to guess word! The rules are simple. I will choose a word, and you will guess the letters. This is not hangman, so you will not be limited to just a few guesses. Are you ready? Let's go.")
+rules = print ("Welcome to guess word! The rules are simple. I will choose a word, and you will guess the letters. You won't be limited to just a few guesses. Are you ready? Let's go.")
 
 import random 
 words = ("sowpods.txt")
