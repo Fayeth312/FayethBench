b86b94875c1f7296dc1ad40c6928b723c961be99
Scholar <scholar@paradigm.local>
2023-11-30 13:11:33 -0700

    hangman

--- hangman.py
+++ hangman.py
@@ -11,28 +11,39 @@ with open('sowpods.txt', 'r') as f:
     line = f.readline()
     words.append(line)
 
-print(random.choice(words))
-
-word = ("Wonderful")
-guessed = "_" * len(word)
+word = random.choice(words)
 word = list(word)
+cleaned_word = []
+for i in word:
+    if i == word[-1]:
+        break
+    else:
+        cleaned_word.append(i)
+guessed = "_" * len(cleaned_word)
 guessed = list(guessed)
 listGuessed = []
-letter = input("guess a letter: ")
+letter = input("guess a letter: ").upper()
+num_guesses = 14
 while True:
     if letter in listGuessed:
         letter = ''
         print("Already guessed!")
-    elif letter in word:
-        index = word.index(letter)
+    elif letter in cleaned_word:
+        index = cleaned_word.index(letter)
         guessed[index] = letter
-        word[index] = '_'
+        cleaned_word[index] = '_'
     else:
         print(''.join(guessed))
         if letter != '':
             listGuessed.append(letter)
-        letter = input("guess a letter: ")
+        letter = input("guess a letter: ").upper()
+        num_guesses -= 1
 
     if '_' not in guessed:
+        print(''.join(guessed))
         print ("You won!")
-        break
+        exit()
+
+    if num_guesses <= 0:
+        print("Sorry you've lost!")
+        exit()
